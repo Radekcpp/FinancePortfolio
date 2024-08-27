073bd6f7107dce62b537426bd5828cd4f1556a50
Radek <bielecki02@gmail.com>
2024-08-27 14:54:22 +0000

    Added connection to database using sequelize

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //Imports
-const express = require('express')
-const axios = require('axios')
+const express = require('express');
+const axios = require('axios');
 
 //Express Definition/Variables
 const app = express()
@@ -8,6 +8,38 @@ const port = 3000
 //const apiKey = "4bb4629b19c8d2e1fd3dd512"
 app.use(express.json());
 
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize('stock', 'root', 'c0nygre', {
+    host: "hsbcpolanddocker1.neueda.com",
+    dialect: 'mysql',
+})
+
+const Stock = sequelize.define('stock_model', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    stock_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    amount_bought: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    date_of_buying: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    price_per_share: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+    },
+    }, {
+        tableName: 'share_information',
+        timestamps: false,
+})
 
 
 app.listen(port, () => console.log(`Exchange app listening on port ${port}!`))
